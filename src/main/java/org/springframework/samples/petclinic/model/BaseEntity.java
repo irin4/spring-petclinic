e49efaea013d21d5c446dc1833268b3ea381e92f
test git <test.git@whatever.com>
2014-10-30 14:24:52 +0100

    test

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -22,12 +22,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
-/**
- * Simple JavaBean domain object with an id property. Used as a base class for objects needing this property.
- *
- * @author Ken Krebs
- * @author Juergen Hoeller
- */
+
 @MappedSuperclass
 public class BaseEntity {
     @Id
